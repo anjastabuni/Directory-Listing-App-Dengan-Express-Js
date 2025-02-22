db5b7e9abbd6e872af20f626248e0b71dfe9d26f
username_anjastabuni <reblytabuni2004@gmail.com>
2025-02-22 15:33:28 +0700

    membuat halaman create dan menyimpan data di db

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ mongoose
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// middleware
+app.use(express.urlencoded({extended: true}))
+
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -28,10 +31,20 @@ app.get("/places", async (req, res) => {
   res.render("places/index", { places });
 });
 
-app.get('/places/:id', async (req, res)=>{
+app.get("/places/create", (req, res) => {
+  res.render("places/create");
+});
+
+app.post("/places", async (req, res) => {
+  const place = new Place(req.body.place);
+  await place.save();
+  res.redirect("/places");
+});
+
+app.get("/places/:id", async (req, res) => {
   const place = await Place.findById(req.params.id);
-  res.render('places/show', {place})
-})
+  res.render("places/show", { place });
+});
 
 // app.get("/seed/place", async (req, res) => {
 //   const place = new Place({
